d49e2a2741fe1e04559f1609365048a12eaa1976
kuhyeon11 <kuhyeon11@gmail.com>
2024-11-21 22:38:08 +0900

    git remote

--- git_remote.sh
+++ git_remote.sh
@@ -4,13 +4,13 @@
 # just for practice
 
 #variables
-repository_name = "git_practice" # your new git repository's name
-my_name = "kuhyeon11" # your git's name
-my_email = "kuhyeon11@gmail.com" # your email's address
-repository_url = "https://github.com/kuhyeon11/git-practice.git"
+repository_name="git_practice" # your new git repository's name
+my_name="kuhyeon11" # your git's name
+my_email="kuhyeon11@gmail.com" # your email's address
+repository_url="https://github.com/kuhyeon11/git-practice.git"
 
-file1_name = "git_init.sh"
-file2_name = "git_remote.sh"
+file1_name="git_init.sh"
+file2_name="git_remote.sh"
 
 # commands
 git add . # add file to staging area
