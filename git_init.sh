1a7d0a87bf7c51f541ee723acfd2d5760bdf3e60
kuhyeon11 <kuhyeon11@gmail.com>
2024-11-21 22:28:40 +0900

    git init

--- git_init.sh
+++ git_init.sh
@@ -7,7 +7,7 @@
 repository_name = "git_practice" # your new git repository's name
 my_name = "kuhyeon11" # your git's name
 my_email = "kuhyeon11@gmail.com" # your email's address
-
+repository_url = "https://github.com/kuhyeon11/git-practice.git"
 # commands
 mkdir $repository_name
 cd $repository_name
@@ -21,3 +21,6 @@ git config --local user.name = $my_name # config local
 git config --local user.email = $my_email
 
 git status # display current status
+
+#after make repository on your git website
+git remote add origin $repository_url
